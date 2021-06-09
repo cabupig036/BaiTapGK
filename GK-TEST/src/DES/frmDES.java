373492f258fc3abd61cca854d4e83b44db8f363e
asxi3011 <71327918+asxi3011@users.noreply.github.com>
2021-06-09 21:20:25 +0700

    add Validation

--- GK-TEST/src/DES/frmDES.java
+++ GK-TEST/src/DES/frmDES.java
@@ -546,6 +546,7 @@ public class frmDES extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(rootPane,"cannot open file");          
             }
         }
+      
     }
     private void writeFile(){
     
@@ -644,6 +645,11 @@ public class frmDES extends javax.swing.JFrame {
         });
 
         jButton2.setText("Generate Key");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jLabel4.setText("Key Generate");
 
@@ -749,6 +755,11 @@ public class frmDES extends javax.swing.JFrame {
     }//GEN-LAST:event_txtPlainTextActionPerformed
 
     private void btnEncryptionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncryptionActionPerformed
+        if(txtPlainText.getText() ==""){
+            JOptionPane.showMessageDialog(rootPane,"Hãy nhập PlainText");
+        }
+        else{
+        
         String plainText = txtPlainText.getText();
         String key = txtKey.getText();
         SecretKey secretKey;
@@ -761,9 +772,15 @@ public class frmDES extends javax.swing.JFrame {
         txtKey1.setText(encodeKey(secretKey));
         String cipherText = encryptText(plainText, secretKey);
         txtCipher.setText(cipherText);
+        }
     }//GEN-LAST:event_btnEncryptionActionPerformed
 
     private void btnDecryptionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDecryptionActionPerformed
+        if(txtCipher.getText() == "")
+        {
+             JOptionPane.showMessageDialog(rootPane,"Hãy nhập CipherText");
+        }
+        else{
         String cipherText = txtCipher.getText();
         String key = txtKey.getText();
         if(key.isEmpty() || cipherText.isEmpty()) return;
@@ -775,6 +792,7 @@ public class frmDES extends javax.swing.JFrame {
 //        SecretKey deKey = decodeKey(encode);
 //        System.out.println(key);
 //        System.out.println(deKey);
+        }
     }//GEN-LAST:event_btnDecryptionActionPerformed
 
     private void txtKey1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtKey1ActionPerformed
@@ -816,6 +834,10 @@ public class frmDES extends javax.swing.JFrame {
         jfrm2.setVisible(true);
         
     }//GEN-LAST:event_formWindowClosing
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+         // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
